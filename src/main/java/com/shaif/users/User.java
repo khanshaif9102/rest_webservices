2cc8594b4ad41e9b4edcad46ad8d5aaf1d71462d
khanshaif9102 <khanshaif9102@gmail.com>
2024-02-19 14:42:48 +0530

    2nd commit

--- src/main/java/com/shaif/users/User.java
+++ src/main/java/com/shaif/users/User.java
@@ -2,9 +2,16 @@ package com.shaif.users;
 
 import java.util.Date;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 	private Integer id;
+	
+	@Size(min=2)
 	private String name;
+	
+	@Past
 	private Date birthDate;
 	public User() {
 		super();
--- src/main/java/com/shaif/users/UserResource.java
+++ src/main/java/com/shaif/users/UserResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class UserResource {
@@ -25,18 +29,32 @@ public class UserResource {
 		return userDao.findALL();
 	}
 	
+//	@GetMapping("/users/{id}")
+//	public User retrieveUser(@PathVariable("id")int id) {
+//		User user= userDao.findOne(id);
+//		
+//		if(user==null) {
+//			throw new UserNotFoundException("id - "+id);
+//		}
+//		return user;
+//	}
+	
 	@GetMapping("/users/{id}")
-	public User retrieveUser(@PathVariable("id")int id) {
-		User user= userDao.findOne(id);
+	public EntityModel<User> retrieveUser(@PathVariable int id){
+		User user=userDao.findOne(id);
+		if(user==null) 
+			throw new UserNotFoundException("id -"+id);
+	
+		EntityModel<User> model=EntityModel.of(user);
 		
-		if(user==null) {
-			throw new UserNotFoundException("id - "+id);
+		WebMvcLinkBuilder linkTo=WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+		
+		model.add(linkTo.withRel("all-users"));
+		return model;
 		}
-		return user;
-	}
 
 	@PostMapping("/users")
-	public ResponseEntity<Object> createUserWithCreatedStatusAndLocation(@RequestBody User user){
+	public ResponseEntity<Object> createUserWithCreatedStatusAndLocation(@Valid @RequestBody User user){
 			User savedUser=userDao.save(user);
 			URI uri=ServletUriComponentsBuilder.fromCurrentRequest()
 					.path("/{id}")
